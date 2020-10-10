e2260a256f48ff63721903a0503bedfc3b3cc10d
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-10 18:46:48 -0300

    Add Menu component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <Header />
-    <router-view></router-view>
+
+    <div class="wrapper">
+      <Menu />
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from 'components/Header'
+import Menu from 'components/Menu'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <aside class="doc__nav">
+    <ul>
+      <li class="js-btn selected">Item 1</li>
+      <li class="js-btn">Item 2</li>
+      <li class="js-btn">Item 3</li>
+      <li class="js-btn">Item 4</li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
