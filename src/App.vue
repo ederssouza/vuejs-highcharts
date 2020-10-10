52dbf868c4788a4a2e87cf56b03c3d7efc55316c
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-10 18:41:11 -0300

    Add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,21 @@
 <template>
   <div id="app">
+    <Header />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from 'components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style lang="scss">
-* {
-  box-sizing: border-box;
-}
-
-body {
-  background-color: #eef3f5;
-  color: #636a6d;
-  font-family: 'Montserrat', sans-serif;
-  padding: 40px 0;
-}
-
-.container {
-  margin: 0 auto;
-  max-width: 960px;
-}
-
-.select__container {}
-
-.select__container:before,
-.select__container:after {
-  clear: both;
-  content: '';
-  display: block;
-  width: 100%;
-}
-
-.select {
-  float: right;
-  padding: 8px;
-}
-
-.container__wrapper {
-  margin-top: 30px;
-  max-width: 100%;
-}
-  /* background-color: #eef3f5;
-  color: #636a6d; */
-.container__wrapper__header {
-  font-weight: 600;
-  padding: 0 0 10px;
-}
+@import 'assets/scss/app.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="header">
+    <h1 class="logo">
+      <span class="logo__thin">Vue.js</span> Highcharts
+    </h1>
+
+    <ul class="menu">
+      <div class="menu__item toggle"><span></span></div>
+      <li class="menu__item">
+        <a href="https://github.com/ederssouza/vuejs-highcharts" class="link link--dark" target="_blank">
+          <i class="fa fa-github"></i> Github
+        </a>
+      </li>
+      <li class="menu__item">
+        <router-link :to="{ name: 'Home' }" class="link link--dark">
+          <i class="fa fa-home"></i> Home
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
