482b33df705df783d253d57d5b19c46492eb3ebc
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-10 19:59:52 -0300

    Add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    Template created exclusively for <a href="https://tympanus.net/codrops/?p=33686" target="_blank" class="link link--light">Codrops</a>.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
