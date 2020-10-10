f5475516ae3d4923142860125fefd491eb772c6d
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-10 20:00:11 -0300

    Add fixedMenu and initScrollSpy methods

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,69 @@
 <template>
   <aside class="doc__nav">
-    <ul>
-      <li class="js-btn selected">Item 1</li>
-      <li class="js-btn">Item 2</li>
-      <li class="js-btn">Item 3</li>
-      <li class="js-btn">Item 4</li>
+    <ul ref="navbar" id="navbar">
+      <li
+        v-for="(item, index) in items"
+        :key="`item${index}`"
+        class="js-btn"
+      >
+        <a
+          :href="`#section${index + 1}`"
+          :title="item.label"
+          :class="{ 'active': index === 0 }"
+        >
+          {{ item.label }}
+        </a>
+      </li>
     </ul>
   </aside>
 </template>
 
 <script>
+import VanillaScrollspy from 'vanillajs-scrollspy'
+
 export default {
-  name: 'Menu'
+  name: 'Menu',
+  data () {
+    return {
+      items: [
+        { label: 'Item 1' },
+        { label: 'Item 2' },
+        { label: 'Item 3' },
+        { label: 'Item 4' }
+      ]
+    }
+  },
+  methods: {
+    fixedMenu () {
+      const navbar = this.$refs['navbar']
+
+      if (navbar) {
+        if (window.pageYOffset === 0) {
+          navbar.querySelector('li:first-child a').classList.add('active')
+        }
+
+        if (window.pageYOffset > 63) {
+          navbar.classList.add('fixed')
+          return
+        }
+
+        navbar.classList.remove('fixed')
+      }
+    },
+    initScrollSpy () {
+      const navbar = this.$refs['navbar']
+      if (navbar) {
+        const scrollspy = new VanillaScrollspy(navbar)
+        scrollspy.init()
+      }
+    }
+  },
+  mounted () {
+    this.initScrollSpy()
+    window.addEventListener('scroll', this.fixedMenu)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.fixedMenu)
+  }
 }
 </script>
