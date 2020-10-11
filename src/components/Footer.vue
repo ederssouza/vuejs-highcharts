5b3515ac70425ccfef67df8216958ef2621743bc
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-11 10:43:44 -0300

    Add footer style and app version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,33 @@
 <template>
   <footer class="footer">
-    Template created exclusively for <a href="https://tympanus.net/codrops/?p=33686" target="_blank" class="link link--light">Codrops</a>.
+    <span class="copyright">
+      VueJS Highcharts v{{ version }} - Template created exclusively for <a href="https://tympanus.net/codrops/?p=33686" target="_blank" class="link link--light">Codrops</a>.</span>
   </footer>
 </template>
 
 <script>
+import { version } from '../../package.json'
+
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  computed: {
+    version () {
+      return version
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: var(--primary-color);
+  color: white;
+  font-size: 12px;
+  padding: 2rem;
+  text-align: center;
+
+  .copyright {
+    display: block;
+  }
+}
+</style>
