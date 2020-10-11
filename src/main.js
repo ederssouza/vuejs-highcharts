3a61dc0fa607b3172166a4f12b186657343da407
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-11 12:34:41 -0300

    Remove hljs module

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import hljs from 'highlight.js'
 import router from './router'
 import './libs/highcharts'
 
-Vue.use(hljs.vuePlugin)
 Vue.config.productionTip = false
 
 new Vue({
