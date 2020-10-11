54b651d466344530d2b1c97af4c698908d36a63e
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-11 10:38:54 -0300

    Add default structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,277 @@
 <template>
   <article class="doc__content">
+    <section class="section" id="section0">
+      <h3 class="section__title">Basic Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <ColumnChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
     <section class="section" id="section1">
-      <h3 class="section__title">Title</h3>
-      <p>Text example.</p>
+      <h3 class="section__title">Area Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <AreaChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section2">
+      <h3 class="section__title">Bar Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <BarChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section3">
+      <h3 class="section__title">Column Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <ColumnChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section4">
+      <h3 class="section__title">Scatter Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <ScatterChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section5">
+      <h3 class="section__title">Line Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <LineChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section6">
+      <h3 class="section__title">Pie Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <PieChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section7">
+      <h3 class="section__title">Combination Chart</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <CombinationChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
+      <hr>
+    </section>
+
+    <section class="section" id="section8">
+      <h3 class="section__title">Rewrite all props</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
+      <ColumnChartSample />
+
+      <h3>Code</h3>
+      <h4>HTML</h4>
+      <highlightjs
+        language='html'
+        :code="`<template>
+  <!-- code here -->
+</template>`"
+      />
+
+      <h4>JavaScript</h4>
+      <highlightjs
+        language='html'
+        :code="`<script>
+export default {
+  data () {
+    // code here
+  }
+}
+</script>`"
+      />
     </section>
   </article>
 </template>
 
 <script>
+import AreaChartSample from 'components/examples/AreaChartSample'
+import BarChartSample from 'components/examples/BarChartSample'
+import ColumnChartSample from 'components/examples/ColumnChartSample'
+import CombinationChartSample from 'components/examples/CombinationChartSample'
+import LineChartSample from 'components/examples/LineChartSample'
+import PieChartSample from 'components/examples/PieChartSample'
+import ScatterChartSample from 'components/examples/ScatterChartSample'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    AreaChartSample,
+    BarChartSample,
+    ColumnChartSample,
+    CombinationChartSample,
+    LineChartSample,
+    PieChartSample,
+    ScatterChartSample
+  }
 }
 </script>
