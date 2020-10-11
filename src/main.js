5eac56e2a6aa73f576bd04ad529b0ea53a5b2706
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-11 10:37:58 -0300

    Import highcharts config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import hljs from 'highlight.js'
 import router from './router'
+import './libs/highcharts'
 
 Vue.use(hljs.vuePlugin)
 Vue.config.productionTip = false
