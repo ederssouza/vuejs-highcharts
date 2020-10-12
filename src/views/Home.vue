6ada6769f404c12a0a59eaaf4b991c81b73e37b9
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-12 09:35:56 -0300

    Change tags

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <article class="doc__content">
-    <section class="section">
+    <section style="padding-top: 10px;">
       <h2 class="section__title">Vue.js Highcharts</h2>
       <p>Vue.js wrapper for Highcharts library.</p>
     </section>
 
     <section class="section">
       <h2 class="section__title">Instalation</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
-      <h3>Code</h3>
       <pre><code class="zsh">$ npm install vuejs-highcharts --save
 # OR
 $ yarn add vuejs-highcharts</code></pre>
-      <hr>
     </section>
 
     <section class="section">
@@ -38,7 +35,6 @@ export default {
   }
 }
 &lt;/script&gt;</code></pre>
-      <hr>
     </section>
 
     <section class="section">
@@ -66,95 +62,91 @@ export default {
           <td>true</td>
         </tr>
       </table>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Basic chart</h2>
+      <h2 class="section__title">Examples</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consequatur, provident mollitia beatae saepe eaque. Quibusdam tempora libero cumque deserunt non assumenda dolore. Sed amet iste velit dignissimos maiores accusantium!</p>
+    </section>
+
+    <section class="section">
+      <h3 class="section__title">Basic chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <ColumnChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Area chart</h2>
+      <h3 class="section__title">Area chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <AreaChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Bar chart</h2>
+      <h3 class="section__title">Bar chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <BarChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Column chart</h2>
+      <h3 class="section__title">Column chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <ColumnChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Scatter chart</h2>
+      <h3 class="section__title">Scatter chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <ScatterChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Line chart</h2>
+      <h3 class="section__title">Line chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <LineChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Pie chart</h2>
+      <h3 class="section__title">Pie chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <PieChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Combination chart</h2>
+      <h3 class="section__title">Combination chart</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <CombinationChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
-      <hr>
     </section>
 
     <section class="section">
-      <h2 class="section__title">Rewrite all props</h2>
+      <h3 class="section__title">Rewrite all props</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus iste iure nulla veritatis possimus aliquam deserunt neque est nihil ullam, corrupti vel eius dignissimos rerum in doloribus pariatur voluptatum.</p>
       <ColumnChartSample />
 
-      <h3>Code</h3>
+      <h4>Code</h4>
       <pre><code class="html"></code></pre>
     </section>
   </article>
