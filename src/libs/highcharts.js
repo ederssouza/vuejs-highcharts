d09006134c3ea23d62ff53edb20f66cd61f2511c
Eder Sampaio <edersampaio@outlook.com.br>
2020-10-20 08:13:01 -0300

    Add 3D module

--- src/libs/highcharts.js
+++ src/libs/highcharts.js
@@ -2,6 +2,8 @@
 
 import Vue from 'vue'
 import Highcharts from 'highcharts'
+import highcharts3D from 'highcharts/highcharts-3d.src'
+highcharts3D(Highcharts)
 
 Vue.$highcharts = Highcharts
 Vue.prototype.$highcharts = Highcharts
